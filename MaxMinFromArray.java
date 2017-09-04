fbf63c1ab7822a05e72177045b5069de5d6526d6
unknown <D:\TheBatMessages>
2017-09-04 15:29:14 +0300

    Релизована задача по массивам

---
+++ MaxMinFromArray.java
@@ -0,0 +1,69 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by Artemka on 04.09.2017.
+ */
+public class MaxMinFromArray {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int arraySize;
+
+        System.out.println("Введите размер масива");
+        arraySize = readPositiveIntValueFromConsole();
+        System.out.println(arraySize);
+        int[] array = new int[arraySize];
+        for (int i = 0; i < array.length; i++){
+            array[i] = (int)(Math.random() * 100);
+        }
+        System.out.println("Массив: " + Arrays.toString(array));
+//        Arrays.sort(array);
+//        System.out.println("Минимальное значение: " + array[0] + ", Максимальное значение: " + array[array.length - 1]);
+        System.out.println("Минимальное значение: " + getMinValueFromArray(array) + ", Максимальное значение: " + getMaxValueFromArray(array));
+
+    }
+
+    // Метод считывания положительных int с консоли
+    public static int readPositiveIntValueFromConsole(){
+        int arraySize = 0;
+        do{
+            while (!scanner.hasNextInt()){
+                System.out.println("Вы неверно ввели число , попробуйте еще раз...");
+                scanner.next();
+            }
+            arraySize = scanner.nextInt();
+            if (arraySize <= 0){
+                System.out.println("Число должно быть положительным");
+            }
+        }while (arraySize <= 0);
+        return arraySize;
+    }
+
+    // Метод получения минимального значения в массиве
+    public static int getMinValueFromArray(int[] array){
+        int minValue = array[0];
+        for (int i = 1; i < array.length; i++){
+            if (array[i] < minValue){
+                minValue = array[i];
+            }
+        }
+        return minValue;
+    }
+
+    // Метод получения максимального значения в массиве
+    public static int getMaxValueFromArray(int[] array){
+        int maxValue = array[0];
+        for (int i = 1; i < array.length; i++){
+            if (array[i] > maxValue){
+                maxValue = array[i];
+            }
+        }
+        return maxValue;
+    }
+
+}
